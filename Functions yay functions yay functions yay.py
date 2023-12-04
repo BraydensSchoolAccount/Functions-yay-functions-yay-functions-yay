b9d80d23f7f63292e40ebb22e46c47bc535ed6c6
brayden <bt22219@students.hcde.org>
2023-12-04 07:20:52 -0500

    finish updating files

--- Functions yay functions yay functions yay.py
+++ Functions yay functions yay functions yay.py
@@ -39,11 +39,66 @@ def format_name(first_name="", last_name=""):
     Returns:
         str: The full name of the user if defined. Otherwise, it returns Anonymous
     """
-    if first_name != "":
+    if first_name:
         return f"{first_name} {last_name}".rstrip()
     # There's not an else because the if statement returns, so if the function is running beyond that, the condition wasn't met
     print("No name passed in")
     return "Anonymous"
+
+def emc_squ(mass, c=299800000):
+    """Gets the energy of a mass
+
+    Args:
+        mass (float): mass of the object in kg
+        c (int, optional): Constant speed of light. Defaults to 2.998*10^8.
+
+    Returns:
+        float: The energy of the object in J
+    """
+    return mass*(c**2)
+
+def student_messenger(name="", grade_level="", school=""):
+    """Greet the students
+
+    Args:
+        name (str, optional): Name of the student. Defaults to "".
+        grade_level (str, optional): Grade of the student, the one we don't obsess over. Defaults to "".
+        school (str, optional): Our families name :). Defaults to "".
+
+    Returns:
+        str: the greeting
+    """
+    if name and grade_level and school:
+        return f'Hello {name} from {school}! Welcome to another day in grade {grade_level}!'
+    if name and grade_level:
+        return f'Hello {name}! Welcome to another day in grade {grade_level}!'
+    if name and school:
+        return f'Hello {name} from {school}!'
+    if grade_level and school:
+        return f'Hello from {school}! Welcome to another day in grade {grade_level}!'
+    if grade_level:
+        return f'Hello! Welcome to another day in grade {grade_level}!'
+    if school:
+        return f'Hello from {school}!'
+    if name:
+        return f'Hello {name}!'
+
+def show_employee(name, salary=9000):
+    """Gives employee what they're due
+
+    Args:
+        name (str): name of the employee
+        salary (float, optional): employee salary. Defaults to 9000.
+
+    Returns:
+        float: How much they're paid
+    """
+    return f'Hey {name}, here\'s your pay of ${salary}'
+
+def reverse(string):
+    reversed_str = ""
+    for i in string:
+        reversed_str = i + reversed_str
 
 next_exercise("Essentially, I have this totally real 100% off coupon")
 
@@ -56,3 +111,17 @@ print(f"Hi there {format_name('James', 'Cameron')}")
 print(f"Hi there {format_name('Greg')}")
 print(f"Hi there {format_name()}")
 
+next_exercise("I'm not Einstein,\n\nThat's it")
+
+print(f"I love the fact {emc_squ(float(input('What is the mass of the object? (kg) >  ')))}J is the energy")
+
+next_exercise("If a school database is running off a terminal, I swear")
+
+print(student_messenger("john", "11th", "SDHS"))
+print(student_messenger( school="SDHS", grade_level="11th", name="john"))
+print(student_messenger("john", school="SDHS", grade_level="11th" ))
+
+next_exercise("WORKWORKWORKWORKWORK hey money WORKWORKWORK...")
+
+print(show_employee("James", 80000))
+print(show_employee("James"))
